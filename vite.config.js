c074c789669a662e5f85fceb23face68580dfa7a
zubair ashiq <zashly.automation@gmail.com>
2026-03-03 07:51:19 -0800

    config: Update base path for ZASH-robo repo

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/animated-robotic-site/',
+  base: '/ZASH-robo/',
   plugins: [react()],
   resolve: {
     alias: {
