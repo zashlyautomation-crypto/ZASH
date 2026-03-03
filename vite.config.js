04c2f0c9b40ccce1004e11c8f8f30a657a6b71c9
zubair ashiq <zashly.automation@gmail.com>
2026-03-03 07:31:13 -0800

    config: Prepare for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  base: '/animated-robotic-site/',
   plugins: [react()],
   resolve: {
     alias: {
