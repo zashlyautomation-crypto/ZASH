9f6ba7299d5a8dcbd867920806120cc7679e3b1f
zubair ashiq <zashly.automation@gmail.com>
2026-03-04 04:19:26 -0800

    hasrouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './store/index.js'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './styles/index.css'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <Provider store={store}>
-        <App />
-    </Provider>
+    <HashRouter>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </HashRouter>
 )
